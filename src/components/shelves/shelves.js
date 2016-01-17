de54dc2be6cb44e11e00db5e9d0d9507569c3147
ZeningQu <quzening@gmail.com>
2016-01-16 16:11:55 -0800

    added marktype icons in shelves;

--- src/components/shelves/shelves.js
+++ src/components/shelves/shelves.js
@@ -13,9 +13,32 @@ angular.module('polestar')
         $scope.schema = vl.schema.schema;
         $scope.pills = Pills;
 
-        $scope.markChange = function() {
-          Logger.logInteraction(Logger.actions.MARK_CHANGE, Spec.spec.marktype);
+        // $scope.markChange = function() {
+        //   Logger.logInteraction(Logger.actions.MARK_CHANGE, Spec.spec.marktype);
+        // };
+
+        $scope.marktypeicons = {
+          'point': {marktype: 'point', icon: 'fa fa-circle-o'}, 
+          'tick': {marktype: 'tick', icon: 'fa fa-square'},
+          'bar': {marktype: 'bar', icon: 'fa fa-bar-chart'},
+          'line': {marktype: 'line', icon: 'fa fa-line-chart'},
+          'area': {marktype: 'area', icon: 'fa fa-area-chart'},
+          'text': {marktype: 'text', icon: 'fa fa-table'}
         };
+
+        // $scope.marktypeicons = [
+        //   {marktype: 'point', icon: 'fa fa-circle-o'}, 
+        //   {marktype: 'tick', icon: 'fa fa-square'},
+        //   {marktype: 'bar', icon: 'fa fa-bar-chart'},
+        //   {marktype: 'line', icon: 'fa fa-line-chart'},
+        //   {marktype: 'area', icon: 'fa fa-area-chart'},
+        //   {marktype: 'text', icon: 'fa fa-table'}];
+
+        $scope.markChange = function(marktype) {
+          Spec.spec.marktype = marktype;
+          Logger.logInteraction(Logger.actions.MARK_CHANGE, Spec.spec.marktype);
+          $scope.marktypedropdown = false;
+        }
 
         $scope.transpose = function(){
           vl.Encoding.transpose(Spec.spec);
